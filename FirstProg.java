a419bcf9cb0711af8bb86c630c75cf3b0067592b
joharris <joharris@zappos.com>
2014-08-04 11:19:26 -0700

    First commit.

---
+++ FirstProg.java
@@ -0,0 +1,10 @@
+public class FirstProg
+{
+		public static void main( String[] args )
+	{
+		System.out.println( "I, John Harris, am determined to learn how to code." );
+		System.out.println( "Today's date is " + "Monday, July 28, 2014." );
+	}
+}
+	
+	
