b2552594ff38a98287a6c29a707a0b62e1b32696
joharris <joharris@zappos.com>
2014-08-04 11:46:22 -0700

    Letter.

---
+++ LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "+---------------------------------------------------------+" );
+		System.out.println( "|                                                      ###|" );
+		System.out.println( "|                                                      ###|" );
+		System.out.println( "|                                                      ###|" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "|                              John Harris                |" );
+		System.out.println( "|                              2432 Parker James Ave      |" );
+		System.out.println( "|                              Henderson, NV 89074        |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "+---------------------------------------------------------+" );
+	}
+}
